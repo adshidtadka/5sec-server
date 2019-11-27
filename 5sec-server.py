9683556cb45a1369b400fbfbb46836dfd6cba1ca
adshidtadka <nextage900fx@gmail.com>
2019-11-27 13:17:02 +0900

    corsを許可する

--- 5sec-server.py
+++ 5sec-server.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
+
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/')
+@cross_origin()
 def hello_world():
     return "Hello World!"
 
