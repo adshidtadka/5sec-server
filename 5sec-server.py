8f28836b77eefb40ce6d440b28e6dd093d006e3e
adshidtadka <nextage900fx@gmail.com>
2019-11-28 13:58:43 +0900

    delete query_db

--- 5sec-server.py
+++ 5sec-server.py
@@ -19,13 +19,6 @@ app.config.from_object(__name__)
 CORS(app)
 
 
-def query_db(query, args=(), one=False):
-    cur = g.db.execute(query, args)
-    rv = [dict((cur.description[idx][0], value)
-               for idx, value in enumerate(row)) for row in cur.fetchall()]
-    return (rv[0] if rv else None) if one else rv
-
-
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
